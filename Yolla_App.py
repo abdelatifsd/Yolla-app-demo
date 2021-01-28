2df310871dd5c022eda3a58d1b72c8e1087e313d
Abdellatif Dalab <aboud@Abdellatifs-MacBook-Pro.local>
2021-01-27 22:01:22 -0500

    Front end changes

--- Yolla_App.py
+++ Yolla_App.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import pickle
+import math
 
 import os
 
@@ -20,11 +21,11 @@ def main_page():
 @app.route('/', methods=['POST'])
 def input_parameters():
     with open("demand-forecasting-model.pkl", 'rb') as file:
-         prediction_model = pickle.load(file)  
+         prediction_model = pickle.load(file)
     item_id = request.form['itemid']
     store_id = request.form['storeid']
     model_input = transform_user_input(int(store_id), int(item_id))
-    prediction = [(prediction_model.predict(model_input)[0])]
+    prediction = [math.floor((prediction_model.predict(model_input)[0]))]
     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'yolla_logo.png')
     return render_template("main_page.html", prediction=prediction, user_image = full_filename)
 
@@ -34,6 +35,4 @@ def transform_user_input(store, item):
 
 
 if __name__ == "__main__":
-     with open("demand-forecasting-model.pkl", 'rb') as file:
-         prediction_model = pickle.load(file)
      app.run(debug=True)
